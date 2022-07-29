6c7a5600edac84e6679f9d1747ed7f0de7472e80
Yasin Hoca <myasinozsaglam@gmail.com>
2022-07-30 01:34:30 +0300

    Add files via upload

---
+++ grafikNo/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace grafikNo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(37, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(387, 375);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(494, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(494, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(520, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ grafikNo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace grafikNo
+{
+    public partial class Form1 : Form
+    {
+
+        int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            /*Graphics g = e.Graphics;
+            g.DrawLine(Pens.Black, new PointF(0f, 0f), new PointF(150f, 150f));
+
+            Pen p = new Pen(Color.FromArgb(255, 0, 0));
+            p.Width = 3;
+
+            g.DrawLine(p, new PointF(20f, 80f), new PointF(200f, 40f));
+
+            p.Color = Color.FromArgb(255, 0, 255);
+            p.Width = 5;
+            g.DrawEllipse(p, new RectangleF(100f,100f,100f,100f));
+            
+        } */
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = Convert.ToString(e.X);
+            label2.Text = Convert.ToString(e.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            panel1.Refresh();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.FromArgb(255, 0, 0));
+            p.Width = 3;
+            g.DrawEllipse(p, new RectangleF(x, y, 10, 10));
+
+
+        }
+
+
+    }
+}
